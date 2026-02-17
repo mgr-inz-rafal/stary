b70dc16e6bf05609b7e2f201c9fbacec33f9eb28
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-17 15:53:35 +0100

    Allow `Access-Control-Allow-Origin: *`

--- galaxy/server.go
+++ galaxy/server.go
@@ -18,8 +18,15 @@ func (s *Server) handleGetPoint(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        next(w, r)
+    }
+}
+
 func (s *Server) Serve() error {
-	http.HandleFunc("/", s.handleGetPoint)
+	http.HandleFunc("/", corsMiddleware(s.handleGetPoint))
 
 	port := ":8081"
 	log.Printf("Server starting on port %s\n", port)
