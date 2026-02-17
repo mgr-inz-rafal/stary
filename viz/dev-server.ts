7ed4b18287ef9284264273064f5c9ced2d3b3236
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-17 17:29:47 +0100

    Rename `server.ts` to `dev-server.ts`

--- viz/dev-server.ts
+++ viz/dev-server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-import { WebSocketServer } from 'ws';
 import * as http from 'http';
 
 const app = express();
@@ -13,8 +12,6 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '.')));
 
 const server = http.createServer(app);
-
-const wss = new WebSocketServer({ server });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
