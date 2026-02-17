9ca5df31ac02813d6b816e466e5f3ff1baad7c59
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-17 18:49:06 +0100

    Remove the superfluous `corsMiddleware`

--- galaxy/server.go
+++ galaxy/server.go
@@ -15,13 +15,6 @@ func (s *Server) handleGetPoint(c *gin.Context) {
 	c.JSON(http.StatusOK, s.world.galaxy)
 }
 
-func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		next(w, r)
-	}
-}
-
 func (s *Server) Serve() error {
 	r := gin.Default()
 	r.Use(cors.Default())
