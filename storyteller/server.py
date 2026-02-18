34938453b02252c3f4b94cc4119421e176b12ad0
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-18 17:36:30 +0100

    Allow CORS in Python server

--- storyteller/server.py
+++ storyteller/server.py
@@ -1,10 +1,18 @@
 import json
 import asyncio
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import httpx
 from anthropic import AsyncAnthropic
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 http_client = httpx.AsyncClient()
 claude = AsyncAnthropic()
