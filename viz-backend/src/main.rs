2c21f7331e207ca620c309d86501ed1797f4cb96
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-20 16:40:23 +0100

    Add proper JWT validation

--- viz-backend/src/main.rs
+++ viz-backend/src/main.rs
@@ -1,10 +1,16 @@
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
+
 use argon2::{Argon2, PasswordHash, PasswordVerifier};
-use axum::{Json, Router, http::StatusCode};
+use axum::{Json, Router, extract::State, http::StatusCode};
 use axum_extra::{TypedHeader, headers};
 use chrono::{Duration, Utc};
-use jsonwebtoken::{EncodingKey, Header};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation};
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
 use tower_http::cors::{AllowOrigin, CorsLayer};
 
 #[allow(dead_code)]
@@ -37,11 +43,16 @@ struct Claims {
 const PASSWORD_HASH: &str =
     "$argon2i$v=19$m=16,t=2,p=1$N0FMbU9yVWR1ODV2eVhYdA$F3uBtcyPeKwFQ6vqlmmxUw";
 
+const TOKEN_SECRET: &str = "my-super-secret";
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), std::io::Error> {
+    let app_state = AppState {};
+
     let app = Router::new()
         .route("/api/v1/story/new", axum::routing::get(fetch_story))
         .route("/login", axum::routing::post(login))
+        .with_state(Arc::new(app_state))
         .layer(
             CorsLayer::new()
                 // TODO: Do not use `any` once auth is up and running
@@ -54,13 +65,17 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 async fn fetch_story(
+    State(_state): State<Arc<AppState>>,
     TypedHeader(auth): TypedHeader<headers::Authorization<headers::authorization::Bearer>>,
 ) -> Result<Json<Story>, StatusCode> {
-    let token = auth.token();
+    let incoming_token = auth.token();
 
-    if !verify_token(token) {
-        return Err(StatusCode::UNAUTHORIZED);
-    }
+    let decoding_key = DecodingKey::from_secret(TOKEN_SECRET.as_ref());
+    let token_data =
+        jsonwebtoken::decode::<Claims>(incoming_token, &decoding_key, &Validation::default())
+            .map_err(|_| StatusCode::UNAUTHORIZED)?;
+
+    println!("User {} authorized to read a story", token_data.claims.sub);
 
     let client = reqwest::Client::new();
 
@@ -79,12 +94,14 @@ async fn fetch_story(
     Ok(Json(story))
 }
 
-fn verify_token(token: &str) -> bool {
-    println!("Verifying token: {token}");
-    false
+struct AppState {
+    // TODO, empty for now
 }
 
-async fn login(Json(payload): Json<LoginRequest>) -> Result<Json<LoginResponse>, StatusCode> {
+async fn login(
+    State(_state): State<Arc<AppState>>,
+    Json(payload): Json<LoginRequest>,
+) -> Result<Json<LoginResponse>, StatusCode> {
     if payload.username != "admin" {
         return Err(StatusCode::UNAUTHORIZED);
     }
@@ -114,7 +131,7 @@ async fn login(Json(payload): Json<LoginRequest>) -> Result<Json<LoginResponse>,
     let token = jsonwebtoken::encode(
         &Header::default(),
         &claims,
-        &EncodingKey::from_secret("my-secret".as_ref()), // TODO: fixme
+        &EncodingKey::from_secret(TOKEN_SECRET.as_ref()),
     )
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
