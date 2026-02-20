1e5cd4b6949533bf9e5278920d3b28c2bbedd64c
mgr_inz_rafal <rchabowski@gmail.com>
2026-02-20 21:32:33 +0100

    Unify all to `camelCase` in proto

--- viz-backend/build.rs
+++ viz-backend/build.rs
@@ -3,6 +3,7 @@ use std::io::Result;
 fn main() -> Result<()> {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    config.type_attribute(".", "#[serde(rename_all = \"camelCase\")]");
     config.compile_protos(&["../proto/types.proto"], &["../proto/"])?;
     Ok(())
 }
